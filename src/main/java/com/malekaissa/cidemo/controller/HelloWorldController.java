fd683f24187932e6f322d329e4cef9412509257d
minnyjoury <workmelek@gmail.com>
2022-12-21 11:46:12 +0100

    init

---
+++ src/main/java/com/malekaissa/cidemo/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.malekaissa.cidemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/")
+	public String hello() {
+		return "hello world";
+	}
+	
+}
+
+
+
+
+
+
+
